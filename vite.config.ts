e646f20558d2536a378544fb79b676acb4268eef
Ben Freijah <ben@launchkeylab.com>
2026-03-25 13:40:55 +1100

    feat: reconfigured config files and added vinxi

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,12 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 export default defineConfig({
-  server: {
-    watch: {
-      usePolling: true,
-      interval: 100,
-    },
-    hmr: true,
-  },
   plugins: [
     tanstackStart({ srcDirectory: "./app" }),
-    react(),
     tailwindcss(),
-    {
-      name: "force-full-reload",
-      handleHotUpdate({ server }) {
-        server.ws.send({ type: "full-reload" });
-      },
-    },
   ],
   resolve: {
     alias: {
