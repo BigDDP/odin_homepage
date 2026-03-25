f2e13f41e449716ca5d74aabef141b5745f2033c
Ben Freijah <ben@launchkeylab.com>
2026-03-25 13:05:17 +1100

    feat: Setup for github pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 export default defineConfig({
+  base: '/odin_homepage/', // must match your repo name exactly
+  build: {
+    outDir: 'dist'
+  },
   server: {
     watch: {
       usePolling: true,
